207eefca51c4514c047f98533c72d51c6de817b8
RiskiAbdi Rahmawan <riskiabdi0806@gmail.com>
2025-05-29 17:40:14 +0700

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landingPage');
 });
+Route::get('/edukasi', function () {
+    return view('educationPage');
+});
